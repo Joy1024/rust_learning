55f4434b036b3588a02ca9e0b789b195408882cb
gaojie <412555203@qq.com>
2025-05-09 18:01:24 +0800

    增加文件索引

--- actix/ws2/src/main.rs
+++ actix/ws2/src/main.rs
@@ -18,6 +18,13 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+
+fn files() -> actix_files::Files {
+    actix_files::Files::new("/static", ".").show_files_listing()
+}
+/**
+ * 运行服务器
+ */
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     
@@ -25,7 +32,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(hello)
             .service(echo)
-            .service(actix_files::Files::new("/static", ".").show_files_listing())
+            .service(files())
             .route("/hey", web::get().to(manual_hello))
     })
     .bind(("127.0.0.1", 8080))?
